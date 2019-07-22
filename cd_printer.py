8793f5b46d2b42b52afe616fc6b7cbee858b9f3a
Jamie Saxon <jsaxon@uchicago.edu>
2019-07-22 18:43:00 -0500

    map printing functions and data

---
+++ cd_printer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python 
+
+import pandas as pd
+import geopandas as gpd
+
+from fiona.crs import from_epsg
+
+import psycopg2
+from netrc import netrc
+user, acct, passwd = netrc().authenticators("harris")
+
+import matplotlib.pyplot as plt
+
+def get_state_cd_map(usps, cd, session = 114, color = "k"):
+
+    g = gpd.GeoDataFrame.from_postgis("""SELECT cd, cd.geom, epsg 
+                                         FROM cd JOIN states ON cd.state = fips
+                                         WHERE states.usps = UPPER('{}') AND cd = {} AND sessn = {};""".format(usps, cd, session),
+                                      con = psycopg2.connect(database = "census", user = user, password = passwd,
+                                                             host = "saxon.harris.uchicago.edu", port = 5432),
+                                      geom_col = "geom", crs = from_epsg(2163), index_col = "cd")
+
+    g = g.to_crs(epsg = g.loc[cd].epsg)
+
+    ax = g.plot(color = color, alpha = 1, linewidth = 0, figsize = (5, 5))
+
+    ax.set_axis_off()
+    ax.figure.savefig("paper_figs/bad_districts/{}_{}_{}.pdf".format(usps, cd, color.replace("#", "")), bbox_inches = 'tight', pad_inches = 0)
+
+
+for st, cd in [["nc", 12], ["oh", 9], ["fl", 5], ["ny", 10], ["tx", 35]]:
+  get_state_cd_map(st, cd)
+
+# get_state_cd_map("nc", 12, color = "#2c72ff")
+# get_state_cd_map("oh",  9, color = "#5a15b5")
+# get_state_cd_map("fl",  5, color = "#18b240")
+# get_state_cd_map("ny", 10, color = "#cc0d0d")
+# get_state_cd_map("tx", 35, color = "#baae15")
+  
+# get_state_cd_map("pa",  7, color = "k")
+
+# get_state_cd_map("tx", 35, color = "b")
+# get_state_cd_map("il",  4, color = "k")
+
---
+++ geomap.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from fiona.crs import from_epsg
+import geopandas as gpd
+from shapely.geometry import Point
+from glob import glob
+import matplotlib.pyplot as plt
+
+def map_format(ax, on = False):
+
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+
+    if not on:
+        ax.set_axis_off()
+        ax.set_axis_on()
+        for a in ["bottom", "top", "right", "left"]:
+            ax.spines[a].set_linewidth(0)
+
+    return ax
+
+
+def make_pa_pittsburgh(f, e = 3364):
+
+    gdf = gpd.read_file(f).to_crs(epsg = e)
+    pittsburgh = gpd.GeoDataFrame(crs = from_epsg(4326), geometry = [Point(-79.9959, 40.4406)]).to_crs(epsg = 3364)
+
+    ax = gdf.plot(column = "id", cmap = "nipy_spectral", alpha = 0.4, figsize = (4, 4))
+    gdf.plot(color = "none", alpha = 1, edgecolor = "k", linewidth = 0.8, ax = ax)
+
+    pittsburgh.plot(facecolor = "none", edgecolor = "k", markersize = 410, ax = ax)
+    pittsburgh.plot(facecolor = "none", edgecolor = "k", markersize = 490, ax = ax)
+    pittsburgh.plot(facecolor = "none", edgecolor = "w", markersize = 450, ax = ax)
+
+    ax.set_xlim(290000, 900000)
+    map_format(ax)
+
+    fname = f.split("/")[-1].replace("geojson", "pdf")
+    ax.figure.savefig("paper_figs/pa_ex/{}".format(fname))
+
+
+for fm in glob("data/pa_ex/*.geojson"):
+    make_pa_pittsburgh(fm)
+
