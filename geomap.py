41f48755cfb6dff194f0ecfbbb71a137ca95e9b9
Jamie Saxon <jsaxon@uchicago.edu>
2019-10-03 10:25:52 -0500

    reverting spacing

--- geomap.py
+++ geomap.py
@@ -25,10 +25,8 @@ def map_format(ax, on = False):
 def make_pa_pittsburgh(f, e = 3364):
 
     gdf = gpd.read_file(f).to_crs(epsg = e)
-
     pittsburgh = gpd.GeoDataFrame(crs = from_epsg(4326), geometry = [Point(-79.9959, 40.4406)]).to_crs(epsg = 3364)
 
-    fig, ax = plt.subplots()
     ax = gdf.plot(column = "id", cmap = "nipy_spectral", alpha = 0.4, figsize = (4, 4))
     gdf.plot(color = "none", alpha = 1, edgecolor = "k", linewidth = 0.8, ax = ax)
 
