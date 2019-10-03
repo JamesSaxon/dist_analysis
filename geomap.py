01605f97551e20276d57221ff9457b3c3d75c7fe
Jamie Saxon <jsaxon@uchicago.edu>
2019-10-03 10:15:44 -0500

    descartes and xlrd required for replication, pdf is messed up with this version, so use png maps

--- geomap.py
+++ geomap.py
@@ -25,8 +25,10 @@ def map_format(ax, on = False):
 def make_pa_pittsburgh(f, e = 3364):
 
     gdf = gpd.read_file(f).to_crs(epsg = e)
+
     pittsburgh = gpd.GeoDataFrame(crs = from_epsg(4326), geometry = [Point(-79.9959, 40.4406)]).to_crs(epsg = 3364)
 
+    fig, ax = plt.subplots()
     ax = gdf.plot(column = "id", cmap = "nipy_spectral", alpha = 0.4, figsize = (4, 4))
     gdf.plot(color = "none", alpha = 1, edgecolor = "k", linewidth = 0.8, ax = ax)
 
@@ -37,8 +39,10 @@ def make_pa_pittsburgh(f, e = 3364):
     ax.set_xlim(290000, 900000)
     map_format(ax)
 
-    fname = f.split("/")[-1].replace("geojson", "pdf")
-    ax.figure.savefig("paper_figs/pa_ex/{}".format(fname))
+    # ax.figure.savefig("paper_figs/pa_ex/" + f.split("/")[-1].replace("geojson", "pdf"))
+    ax.figure.savefig("paper_figs/pa_ex/" + f.split("/")[-1].replace("geojson", "png"))
+
+    plt.close("all")
 
 
 for fm in glob("data/pa_ex/*.geojson"):
